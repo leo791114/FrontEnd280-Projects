2c3027f1894bf7bbcddfac2c9c26a7632b2511c9
leo791114 <leo791114@hotmail.com>
2017-03-28 19:53:57 +0800

    update pokemon search function

--- JS280-Lecture08-Examples/Pokemon_HW/pokemon.js
+++ JS280-Lecture08-Examples/Pokemon_HW/pokemon.js
@@ -10,3 +10,20 @@ var pokemons = [
     { name: 'Hitokage', nationalNo: 2 }
 ];
 
+app.get('/pokemon', function (req, res) {
+    if (req.query.nationalNo != null) {
+        var nationalNo = req.query.nationalNo;
+        for (var index = 0; index < pokemons.length; index++) {
+            if (nationalNo == pokemons[index].nationalNo) {
+                pokemon = pokemons[index];
+            } else {
+                res.render("query");
+            }
+        }
+    } else {
+        res.render('query');
+    }
+    res.render('pic', pokemon);
+    res.end();
+
+});
